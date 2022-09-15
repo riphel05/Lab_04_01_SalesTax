406d23e63f3d39177927a8509192d19b54ef1194
riphel05 <phelpsre@mail.uc.edu>
2022-09-15 17:30:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class
+Main {
+    public static void main(String[] args)
+    {
+
+    double purPrice = 63.99;
+    double shippingPrice;
+
+    shippingPrice = purPrice * .05;
+
+        System.out.println("The shipping cost for the provided item is " + shippingPrice);
+
+
+
+
+
+
+
+    }
+}
